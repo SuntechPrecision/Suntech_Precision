9b62e00d09d39bb8d5b1a916454f864d67d16041
Tanmoy Saha <tanmoytssaha@gmail.com>
2023-03-23 00:15:16 +0530

    Adding sliding animations

--- script.js
+++ script.js
@@ -6,4 +6,74 @@ window.onload = function () {
         menu_btn.classList.toggle('is-active');
         menu_list.classList.toggle('is-active');
     });
+
+    //JS code for sliding animation
+    const btns = document.querySelectorAll(".nav-btn");
+    const slides = document.querySelectorAll(".img-slide");
+
+    var sliderNav = function(manual) {
+        btns.forEach((btn)=>{
+            btn.classList.remove("btn-active");
+        });
+        
+        slides.forEach((slide)=>{
+            slide.classList.remove("slide-active");
+        });
+
+        btns[manual].classList.add("btn-active");
+        slides[manual].classList.add("slide-active");
+    }
+
+    btns.forEach((btn, i) => {
+        btn.addEventListener("click", ()=>{
+            sliderNav(i);
+        });
+    });
+
+    // const leftArrow = document.querySelector(".arrow-left")
+    // const rightArrow = document.querySelector(".arrow-right")
+
+    // rightArrow.addEventListener("click", function(e) {
+    //     e.preventDefault();
+    //     nextSlide();
+    // });
+
+    // leftArrow.addEventListener("click", function (e) {
+    //     e.preventDefault()
+    //     prevSlide();
+    // });
+
+    // function nextSlide() {
+    //     console.log(slides);
+    //     var active = document.querySelector(".slide-active");
+    //     var dot_btns = document.querySelector(".btn-active");
+    //     // Unset Current Slide and Radio Button
+    //     active.classList.remove("slide-active");
+    //     dot_btns.classList.remove("btn-active");
+    //     // Set Next Slide and Radio Button
+    //     if (active.nextElementSibling) {
+    //         dot_btns.nextElementSibling.classList.add("btn-active");
+    //         active.nextElementSibling.classList.add("slide-active");
+    //     } else if(dot_btns.length <= 3){
+    //         dot_btns[0].classList.add("btn-active");
+    //         slides[0].classList.add("slide-active");
+    //     }
+    //   }
+
+    //   function prevSlide() {
+    //     console.log(slides)
+    //     var active = document.querySelector(".slide-active");
+    //     var dot_btns = document.querySelector(".btn-active");
+    //     // Unset Current Slide and Radio Button
+    //     active.classList.remove("slide-active");
+    //     dot_btns.classList.remove("btn-active");
+    //     // Set Next Slide and Radio Button
+    //     if (active.previousElementSibling) {
+    //       active.previousElementSibling.classList.add("slide-active");
+    //       dot_btns.nextElementSibling.classList.add("btn-active");
+    //     } else {
+    //       slides[slides.length - 1].classList.add("slide-active");
+    //       dot_btns[slides.length - 1].classList.add("btn-active");
+    //     }
+    //   } 
 }
